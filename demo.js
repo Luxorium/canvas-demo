ab936469b5108c852293a0e1a4f4f993c53e60e4
Caleb <cdaix10@gmail.com>
2019-10-03 08:37:44 -0500

    Update demo.js

--- demo.js
+++ demo.js
@@ -5,3 +5,26 @@ canvas.style.display = "block";
 const context = canvas.getContext("2d");
 document.body.appendChild(canvas);
 document.body.style.margin = 0;
+
+function init() {
+}
+
+function update() {
+}
+
+function render() {
+  context.fillStyle = "black";
+  context.fillRect(0,0,100,100);
+}
+
+
+
+
+
+function animate() {
+  requestAnimationFrame(animate);
+  update();
+  render();
+}
+init();
+animate();
